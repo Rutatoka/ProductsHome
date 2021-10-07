9dd51010f062a749959f3d3a88544341236aaad4
Angelina <lunina.angelinka2002@gmail.com>
2021-10-07 20:50:32 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
-let products=[];
+
+let table  = document.querySelector('.table');
 document.querySelector('#oke').onclick=function payProducts(items){
+    recountTotal();
 items={
 name: document.querySelector('#name').value,
 quantity: document.querySelector('#quantity').value,
-price: document.querySelector('#price').value
  }
- console.log(items)
 
-let parentOfText= document.querySelector('#parentOfText');
-let textArea= document.createElement('li');
-parentOfText.appendChild(textArea)
-textArea.innerHTML='Название:   '+[items.name]+' Количество:   '+[items.quantity]+' Цена:   '+[items.price];
-let amount=document.querySelector("#amount");
-let a;
-a=(+items.quantity)*(+items.price);
-let result=0;
-result+=Number(a);
-amount.innerHTML=result;
 
+ let ol = document.createElement('ol');
+ let quantities=document.createElement('span');
+ let names=document.createElement('div');
+ let li = document.createElement('li')
+ quantities.innerHTML = items.quantity;
+ names.innerHTML = items.name;
+
+
+ ol.append(li);
+ li.append(names);
+ li.append(quantities);
+ table.append(li);
+ document.querySelector('#amount').innerHTML=recountTotal();
 }
 
+
+function recountTotal() {
+	let spisok=document.querySelectorAll('table li span');
+	let result=0;
+	for(let elem of spisok){
+	result+=Number(elem.innerHTML);
+	}
+	document.querySelector('#amount').innerHTML=result;
+	console.log(result);
+	return result;
+}
