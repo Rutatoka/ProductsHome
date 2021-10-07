8f34f2b1dc13298551ea06eb988c0f1d921f471a
Angelina <lunina.angelinka2002@gmail.com>
2021-10-07 20:03:00 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 let products=[];
-
-function payProducts(items){
+document.querySelector('#oke').onclick=function payProducts(items){
 items={
-name:'имя продукта',
-quantity:'количество продуктов'
+name: document.querySelector('#name').value,
+quantity: document.querySelector('#quantity').value,
+price: document.querySelector('#price').value
  }
  console.log(items)
+
+let parentOfText= document.querySelector('#parentOfText');
+let textArea= document.createElement('li');
+parentOfText.appendChild(textArea)
+textArea.innerHTML='Название:   '+[items.name]+' Количество:   '+[items.quantity]+' Цена:   '+[items.price];
+let amount=document.querySelector("#amount");
+let a;
+a=(+items.quantity)*(+items.price);
+let result=0;
+result+=Number(a);
+amount.innerHTML=result;
+
 }
+
